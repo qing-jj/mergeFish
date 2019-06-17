fdead28b3fc5545368c23141a89de63b2e8def83
melody <zhuzhijun@shenwainfo.com>
2019-06-17 20:12:34 +0800

    0.0

--- main.b3e17.js
+++ main.b3e17.js
@@ -144,7 +144,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ca6d5.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6d2ad.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -180,7 +180,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.95630.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.b3e17.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -207,12 +207,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.5caa2.js');
+        require('src/settings.498e4.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.5caa2.js');
+        require('src/settings.498e4.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
